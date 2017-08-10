923f5888f14d3586860be3cc0355af9c0aa36ade
joao <jnvalente@hotmail.com>
2017-08-10 17:11:36 +0100

    Horario Section: - Image fit;

--- app/src/main/java/com/example/bestaveiro/appcurso/HorarioEvento.java
+++ app/src/main/java/com/example/bestaveiro/appcurso/HorarioEvento.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 
 public class HorarioEvento extends Fragment
@@ -23,7 +22,7 @@ public class HorarioEvento extends Fragment
 
         image = (ImageView) myView1.findViewById(R.id.imageView1);
         image.setImageResource(R.mipmap.schedule);
-        image.setRotation(image.getRotation()+ 90);
+//        image.setRotation(image.getRotation()+ 90);
 
         return myView1;
     }
